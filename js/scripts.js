4a448cde76d7cc4f63c01a70ee2533e5ce9d9f1c
John Doe <john.doe@example.com>
2017-05-08 22:55:31 +0200

    style.css index.html scripts.js modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,10 @@
 function checkOnclickEvent(param) {
   console.log(param);
 }
-var list = document.getElementById('list'),
-    add = document.getElementById('addElem');
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
 
-add.addEventListener('click', function(list) {
-  checkOnclickEvent('button został kliknięty')
+add.addEventListener('click', function() {
+  var number = document.getElementsByTagName('li');
+  list.innerHTML += '<li>item ' + number.length +'</li>'
 });
-
-list.addEventListener('list', function(list) {
-  list.innerHTML += '<li>item</li>'
-});
-console.log(list);
-
-var number = document.getElementsByTagName('item');
-for (var i = 0; i < number.length; i++) {
-	var numbr = number[i].innerText;
-	console.log('Item: ' + numbr);
-}
