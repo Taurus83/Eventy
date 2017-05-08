5dc2c017fc14568a9f8930933a8fecc4a8d65759
John Doe <john.doe@example.com>
2017-05-08 21:09:37 +0200

    js/scripts.js modified

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ function checkOnclickEvent(param) {
 var list = document.getElementById('list'),
     add = document.getElementById('addElem');
 
-add.addEventListener('click', function() {
+add.addEventListener('click', function(list) {
   checkOnclickEvent('button został kliknięty')
 });
 
-list.addEventListener('list', function() {
-  list.innerHTML += '<li>item</li>';
+list.addEventListener('list', function(list) {
+  list.innerHTML += '<li>item</li>'
 });
 console.log(list);
 
